0a90abfb97de2b1059343b35abc495eafafdf365
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2023-08-16 01:20:15 -0600

    Add files via upload

---
+++ examples/example.js
@@ -0,0 +1,54 @@
+//Register Script:
+script.registerScript({
+    name: "TestScript",
+    version: "1.0",
+    description: "The Test!",
+    website: "https://github.com/Trollhunters501/JSEngineNK/",
+    authors: ["Creadores Program & RedstoneAlmeida"]
+});
+//You can register your script so that it appears in the list of scripts with the command /scripts or also with the command /version or /ver
+//The mandatory parameters are: author or authors, name, version and description optional: website
+
+//Commands:
+manager.createCommand("name", "descricao do comando", "functiondele");
+// o name = Nome do Comando, descrição do comando, functiondela é a função que será executada
+// ao digitar o comando!
+
+function functiondele(sender, args){
+    if(args.length < 1){ // praticamente o isset do PHP
+        sender.sendMessage('Você digitou incorretamente');
+        return;
+    }
+    var name = args[0];
+    sender.sendMessage("Você digitou: " + name); // formata para adicionar mensagens
+    // sender.sendMessage(manager.format("Você digitou: %s", name)); outra forma de adicionar mensagens com {NAME}
+}
+
+//Configs:
+var config = manager.createConfig(manager.getFile("test", "config.yml"), 2);
+
+config.set("test", true);
+config.save();
+
+//Events:
+script.addEventListener("Enable", function(){
+    console.info("Hello World!");
+});
+script.addEventListener("ItemFrameDropItemEvent", function(event){
+    console.info("Oki");
+});
+//When registering an event you will use this function! The first parameter is the event when it is executed and the second is the function that will be executed!
+//Visit https://github.com/Trollhunters501/JSEngineNK/blob/master/Events%20Soported/Events.md to see Supported events
+
+//Tasks:
+manager.createTask("tasknormal", 20);
+// atime is by minecraft ticks
+manager.createLoopTask("taskloop", 20); // atime is by minecraft ticks 
+
+function tasknormal(currentTick){
+    print('sou tasknormal!');
+}
+
+function taskloop(currentTick){
+    print('sou taskloop');
+}
