3ba213652155f7ec6de10ef04dc5b548425852a5
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2023-08-16 01:19:51 -0600

    Delete events.js
    No longer exists

--- examples/events.js
+++
@@ -1,8 +0,0 @@
-function PlayerJoinEvent(event){
-    var player = event.getPlayer();
-    player.sendMessage("Bem-Vindo ao Servidor!");
-}
-
-// basta criar uma function
-// function BlockBreakEvent(event){}
-// e pronto o evento já será executado!
