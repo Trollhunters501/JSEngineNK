96a515b6b0a98e90579e00cf065ebd3f004c47ce
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2023-11-03 12:28:51 -0600

    Update task.js

--- examples/task.js
+++ examples/task.js
@@ -1,6 +1,6 @@
-manager.createTask("tasknormal", 20 * 20);
-// tasknormal é a function, e 20 * 20 é o tempo = 20 segundos, será executado depois de 20 segundos
-manager.createLoopTask("taskloop", 20 * 20); // tempo de 20 em 20 segundos será executado
+manager.createTask("tasknormal", 20);
+// tasknormal é a function, e 20 é o tempo = 20 tiks
+manager.createLoopTask("taskloop", 20); // tempo de 20 ticks será executado
 // taskloop é a function
 
 function tasknormal(currentTick){
