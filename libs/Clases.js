63ab8bbe1d273c35d5513c9ff182e0d73154686b
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2024-01-06 12:44:00 -0600

    Update Clases.js
    New libs

--- libs/Clases.js
+++ libs/Clases.js
@@ -1,7 +1,9 @@
+load("https://cdnjs.cloudflare.com/ajax/libs/es6-shim/0.35.5/es6-sham.min.js");
+load("https://cdn.jsdelivr.net/npm/unorm@1.6.0/lib/unorm.min.js");
 const Class = function ($superclass, config) {
     // All classes have a superclass with the root 
     // of this $class hierarchy being Object.
-    var self = function (config) {
+    let self = function (config) {
         // Object.assign or $.extend or ...
         config && Object.assign(this, config);
     };
