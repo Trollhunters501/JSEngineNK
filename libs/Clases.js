ca1c540c9409717167d550353e27590580abad33
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2025-01-12 10:18:40 -0600

    Update Clases.js

--- libs/Clases.js
+++ libs/Clases.js
@@ -1,12 +1,11 @@
 load("https://cdnjs.cloudflare.com/ajax/libs/es6-shim/0.35.5/es6-sham.min.js");
 load("https://cdn.jsdelivr.net/npm/unorm@1.6.0/lib/unorm.min.js");
-const Class = function ($superclass, config) {
+const Class = function ($superclass, config, configStatic) {
     // All classes have a superclass with the root 
     // of this $class hierarchy being Object.
     let self = function (config) {
         // Object.assign or $.extend or ...
         config && Object.assign(this, config);
-        this.$super = Object.create($superclass.prototype);
         Object.getOwnPropertyNames($superclass.prototype).forEach(function(methodName){ 
             if (typeof this.$super[methodName] === 'function') { 
                 this.$super[methodName] = this.$super[methodName].bind(this);
@@ -14,7 +13,11 @@ const Class = function ($superclass, config) {
         }.bind(this));
         this.$constructor && this.$constructor(config);
     };
-    config && (self.prototype = Object.assign(Object.create($superclass.prototype), config));
+    Object.assign(self, $superclass);
+    configStatic && Object.assign(self, configStatic);
+    self.prototype = Object.create($superclass.prototype);
+    config && Object.assign(self.prototype, config);
     self.prototype.constructor = self;
+    self.prototype.$super = $superclass.prototype;
     return self;
 };
